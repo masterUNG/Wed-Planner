7b061febc9d5d6b9ad7fd0789f677b0fe8d380d1
masterUNG <phrombutr@gmail.com>
2016-04-25 11:27:54 +0700

    Check Space Finish

--- app/src/main/java/chairattanangkul/ployrung/wedplanner/SignUpActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/wedplanner/SignUpActivity.java
@@ -3,17 +3,42 @@ package chairattanangkul.ployrung.wedplanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText emailEditText, passwordEditText;
+    private String emailString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        emailEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+
+
     }   // Main Method
 
     public void clickSignUpSign(View view) {
 
-    }
+        emailString = emailEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (emailString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
+
+        } else {
+            //No Space
+
+        }
+
+    }   // clickSignUp
 
 }   // Main Class
