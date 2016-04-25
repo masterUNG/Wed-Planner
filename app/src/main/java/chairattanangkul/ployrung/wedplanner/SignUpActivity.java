6ed8bd205ee55049a7214e62799204ca4e204d0c
masterUNG <phrombutr@gmail.com>
2016-04-25 11:04:37 +0700

    Design UI Signup finish

--- app/src/main/java/chairattanangkul/ployrung/wedplanner/SignUpActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/wedplanner/SignUpActivity.java
@@ -2,6 +2,7 @@ package chairattanangkul.ployrung.wedplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
     }
-}
+
+}   // Main Class
